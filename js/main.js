034fe2221b5cf6eaa969c1adbf62263fc6a0e0d3
Nader Khamis <nadersjob@gmail.com>
2021-01-23 20:13:13 +0300

    Nader added his task - Top Bar, Header, Slider and Footer

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    setInterval(function() {
+        $('.deal').fadeIn(100);
+        $('.deal').fadeOut(100);
+    }, 300);
+});
